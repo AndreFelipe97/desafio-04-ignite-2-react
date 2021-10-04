f7e61dcbea41c3fb29f76de2f60f3156e45fc876
Andre Felipe Rodrigues de freitas <andrefeliperf17@gmail.com>
2021-10-04 00:42:40 -0300

    finalizaçao do desafio

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,24 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <title>Blog</title>
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=spacetraveling2"
+          />
         </body>
       </Html>
     );
